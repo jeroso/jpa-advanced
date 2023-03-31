b54efd3bf3681a03a545ff567cb2f654df4e3f7e
woodsy <jeroso4481@gmail.com>
2023-03-31 23:33:25 +0900

    frist commit

---
+++ src/main/java/com/jpa/advanced/JpaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvancedApplication.class, args);
+	}
+
+}
